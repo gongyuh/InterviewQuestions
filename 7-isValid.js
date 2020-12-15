8250f63bc0654e8e607920aa44bc0240543fd932
gongyuh <2736498007@qq.com>
2020-12-15 22:46:44 +0800

    有效括号检测

---
+++ 7-isValid.js
@@ -0,0 +1,24 @@
+
+var isValid = function(s) {
+    const mappings = new Map();
+    mappings.set("(",")");
+    mappings.set("[","]");
+    mappings.set("{","}");
+
+    const stack = [];
+    for(let i=0;i<s.length;i++){
+        if(mapping[i].has(s[i])){
+            stack.push(mappings.get(s[i]));
+        }else{
+            if(stack.pop() !== s[i]){
+                return false;
+            }
+        }
+    }
+
+    if(stack.length !== 0){
+        return false;
+    }
+
+    return true;
+};
